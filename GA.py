8fbc409af7882d8f92748e32996efdf19bbcc7ae
Grant Zhao <grantzhao6@gmail.com>
2024-03-28 17:15:18 -0400

    fix

--- GA.py
+++ GA.py
@@ -5,11 +5,6 @@ from functools import reduce
 import numpy as np
 import math 
 
-print("hello")
-#CHenguw
-# hello
-#dklsjdfksdjflskdj 
-
 #create a memeber of the population
 def individual (length , min , max) :
     return [randint(min, max) for x in range(length)]
